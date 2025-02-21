81a2b130b4510e97edd028db90ad8e61fad86091
BrendahBanks <ampurirebrendah7@gmail.com>
2025-02-21 08:53:52 +0300

    group

---
+++ question_8.js
@@ -0,0 +1,9 @@
+//Write a JavaScript function that checks if a number is even or odd. The user should enter a number, and the result should be displayed as "Even" or "Odd."
+
+
+function checkEvenOdd() {
+    return number %2 === 0 ? "Even":"odd"
+}
+
+console.log(isEvenOrOdd(7));//out put is odd
+console.log(isEvenOrOdd(10));//Output is even 
