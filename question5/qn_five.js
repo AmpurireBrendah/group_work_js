fa9fa5c34f1dddacbee9de821abb4737a3c7e2b2
Faith Namono <faithnamono4@gmail.com>
2025-02-21 23:24:20 +0300

    question 5 and 6

---
+++ question5/qn_five.js
@@ -0,0 +1,7 @@
+// Count Words in a Sentence
+function countWords(sentence) {
+    return sentence.split(" ").length;
+}
+
+
+console.log(countWords("Hello world, welcome to JavaScript!")); // Output is 5
